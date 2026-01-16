9855a1646e760f9a9e29debe5f7ff8226977d77e
Alex Tetervak <tetervak@sheridancollege.ca>
2026-01-16 10:07:39 -0500

    removed unnecessary code

--- src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
+++ src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
@@ -2,8 +2,6 @@ package ca.tetervak.hellouser.controller;
 
 import ca.tetervak.hellouser.model.AppUser;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
